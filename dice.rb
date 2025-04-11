480bf0b1f02f643a89d5989c8a790f5a0c4b3ff6
martaf793 <martaf@uchicago.edu>
2025-04-11 22:01:46 +0000

    embedded ruby

--- dice.rb
+++ dice.rb
@@ -8,14 +8,15 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "<h1>Let's play</h1>
-  <ul>
-    <li><a href=\"https://turbo-computing-machine-r4j4ppvpv9q25jq-4567.app.github.dev/dice/2/6\" target=\"_blank\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-  <li><a href=\"/dice/1/20\">Roll one 20-sided dice</a></li>
-  <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-</ul>
-  "
+#   "<h1>Let's play</h1>
+#   <ul>
+#     <li><a href=\"https://turbo-computing-machine-r4j4ppvpv9q25jq-4567.app.github.dev/dice/2/6\" target=\"_blank\">Roll two 6-sided dice</a></li>
+#     <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+#   <li><a href=\"/dice/1/20\">Roll one 20-sided dice</a></li>
+#   <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+# </ul>
+#   "
+  erb(:lion)
 end 
 
 get("/zebra") do
@@ -30,25 +31,28 @@ get("/dice/2/6") do
   n1=rand(1..6)
   n2=rand(1..6)
   sum=n1+n2
-  outcome="You got #{n1} and #{n2}, adding #{sum}"
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  @outcome="You got #{n1} and #{n2}, adding #{sum}"
+  # "<h1>2d6</h1>
+  # <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
     n1=rand(1..10)
     n2=rand(1..10)
     sum=n1+n2
-    outcome="You got #{n1} and #{n2}, adding #{sum}"
-    "<h1>2d10</h1>
-    <p>#{outcome}<p/>"
+    @outcome="You got #{n1} and #{n2}, adding #{sum}"
+    # "<h1>2d10</h1>
+    # <p>#{outcome}<p/>"
+    erb(:two_ten)
 end
 
 get("/dice/1/20") do
   n1=rand(1..20)
-  outcome="You got #{n1}"
-  "<h1>1d20</h1
-  <p>#{outcome}</p>"
+  @outcome="You got #{n1}"
+  # "<h1>1d20</h1
+  # <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -58,7 +62,8 @@ get("/dice/5/4") do
   n4=rand(1..4)
   n5=rand(1..4)
   sum=n1+n2+n3+n4+n5
-  outcome="You got #{n1}, #{n2}, #{n3}, #{n4} and #{n5}, adding #{sum}"
-"<h1>5d4</h1>
-<p>#{outcome}</p1>"
+  @outcome="You got #{n1}, #{n2}, #{n3}, #{n4} and #{n5}, adding #{sum}"
+# "<h1>5d4</h1>
+# <p>#{outcome}</p1>"
+  erb(:five_four)
 end
